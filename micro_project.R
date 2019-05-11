a1d7d18e52da957b161a754934e817fa2d9fcc9e
Mohammad Ehtasham Billah <mymun.stat@gmail.com>
2019-05-11 17:42:50 +0200

    Update micro_project.R

--- micro_project.R
+++ micro_project.R
@@ -26,12 +26,8 @@ registerDoSNOW(cl)                                     # Registering clusters
 
 df <- read.delim('NIHMS163142-supplement-01.txt', sep = ",")
 colnames(df)
-View(df[is.na(df),])
 
-str(df)
-unique(df$first_total_pmc_citations)
-hist(df$first_total_pmc_citations)
-
+# Converting numeric to categorical
 df$pmid <- factor(df$pmid)
 df$policy_strength <- factor(df$policy_strength, ordered = T)
 df$is_usa_address <- factor(df$is_usa_address)
@@ -47,12 +43,11 @@ df$is_data_shared <- factor(df$is_data_shared)
 imputed_df <- mice(df, m=1, maxit = 200, method = 'cart', seed = 2019)
 summary(imputed_df)
 df_final <- complete(imputed_df,1)
-sum(is.na(df_final))
 levels(df_final$is_data_shared) <- c("no", "yes")
 df_final$is_usa_address <- factor(df_final$is_usa_address)
 
 #### Compute principal components of author experience
-### FIRST AUTHOR
+# FIRST AUTHOR
 df_final$first_career_length = 2008 - df_final$first_first_year
 df_final$last_career_length = 2008 - df_final$last_first_year
 
@@ -60,8 +55,6 @@ pc_first = princomp(scale(cbind(log(1+df_final$first_hindex),
                                 log(1+df_final$first_aindex), 
                                 df_final$first_career_length)))
 summary(pc_first)
-# NOTE THE NEGATIVE!  This is so that
-# higher hindexes etc correlate with higher author experience scores
 first_author_exp = - pc_first$scores[,1]  
 pc_first$loadings
 #biplot(pc.first)
@@ -72,8 +65,6 @@ pc_last = princomp(scale(cbind(log(1+df_final$last_hindex),
                                log(1+df_final$last_aindex), 
                                df_final$last_career_length)))
 summary(pc_last)
-# NOTE THE NEGATIVE!  This is so that
-# higher hindexes etc correlate with higher author experience scores
 last_author_exp = - pc_last$scores[,1]
 pc_last$loadings
 #biplot(pc.last)
@@ -102,6 +93,7 @@ optimum_features <- optimum_features[-1]
 df_lasso <- df_final[,c("impact_factor","policy_strength","first_hindex", "is_data_shared")]
 
 # LOGISTIC REGRESSION
+# model used by the author
 logistic_model_org <- glm(is_data_shared ~ policy_strength + 
                             is_usa_address * is_nih_funded +
                             any_nih_data_sharing_applies +
@@ -110,7 +102,7 @@ logistic_model_org <- glm(is_data_shared ~ policy_strength +
                             log(impact_factor),
                           data = df_final, family = "binomial")
 
-
+# model with variables selected by LASSO
 logistic_model_lasso <- glm(is_data_shared ~ impact_factor + policy_strength +
                               first_hindex, data = df_lasso, family = "binomial")
 
@@ -136,7 +128,6 @@ anova(logistic_model_org,logistic_model_lasso,test = "Chisq")
 # A. Original model
 probabilities_org <- predict(logistic_model_org,type = "response")
 predicted_classes_org <- ifelse(probabilities_org > 0.5, "yes", "no")
-
 
 # 1. linearity
 # selecting only the covariates used in original model
